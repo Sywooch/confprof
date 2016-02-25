e1c1b5001faae77007f837ac7689c22edb56b034
Victor <devmosedu@yandex.ru>
2016-02-25 15:48:32 +0300

    Вынес статистику на главную

--- themes/admin/site/index.php
+++ themes/admin/site/index.php
@@ -1,13 +1,24 @@
 <?php
 
 use yii\bootstrap\Html;
+use app\components\Statistics;
 
 /* @var $this yii\web\View */
 
 $this->title = Yii::$app->name;
+
+echo $this->render(
+    '//report/statistics',
+    [
+        'data' => Statistics::getConferenceStat(),
+    ]
+);
+/*
+<p><?= Html::encode($this->title) ?></p>
+<p><?= Html::encode(Yii::$app->controller->layout) ?></p>
+*/
+
 ?>
-    <p><?= Html::encode($this->title) ?></p>
-    <p><?= Html::encode(Yii::$app->controller->layout) ?></p>
 
 
 <!--Остальные страницы-->
