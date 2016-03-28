de8f1b0976c451c6269ce837b4f28db920ab85a3
Victor <devmosedu@yandex.ru>
2016-03-28 12:40:22 +0300

    Change download file size

--- config/params.php
+++ config/params.php
@@ -4,6 +4,6 @@ return [
     'adminEmail' => 'no-reply@profil.mos.ru',
     'fromEmail' => 'no-reply@profil.mos.ru',
     'doclad.file.ext' => ['ppt', 'pptx', 'zip', ],
-    'doclad.file.maxsize' => 1000000,
+    'doclad.file.maxsize' => 5000000,
     'doclad.description.maxlength' => 2000,
 ];
