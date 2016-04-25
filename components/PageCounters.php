ab404f3d12eaf84fc84b0fcfe77e2109cb1f7e81
Victor <devmosedu@yandex.ru>
2016-04-25 11:27:03 +0300

    Добалена метрика yandex

--- components/PageCounters.php
+++ components/PageCounters.php
@@ -21,6 +21,35 @@ class PageCounters {
         screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
     ";"+Math.random();//--></script><!--/LiveInternet-->
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+(function (d, w, c) {
+(w[c] = w[c] || []).push(function() {
+try {
+w.yaCounter36936810 = new Ya.Metrika({
+id:36936810,
+clickmap:true,
+trackLinks:true,
+accurateTrackBounce:true
+});
+} catch(e) { }
+});
+
+var n = d.getElementsByTagName("script")[0],
+s = d.createElement("script"),
+f = function () { n.parentNode.insertBefore(s, n); };
+s.type = "text/javascript";
+s.async = true;
+s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+if (w.opera == "[object Opera]") {
+d.addEventListener("DOMContentLoaded", f, false);
+} else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/36936810" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 EOT;
 
     }
